0797fe8e825153d6af123ecd414096f6546f362e
Harshshah6 <harshsandeep23@gmail.com>
2025-01-09 18:15:20 +0530

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   src/controllers/User.ts 	new file:   src/index.ts 	new file:   src/models/UserModel.ts 	new file:   tsconfig.json

---
+++ src/controllers/User.ts
@@ -0,0 +1,5 @@
+import User from "@/models/UserModel";
+
+const user:User = new User("John Doe", 30);
+
+export default user;
---
+++ src/models/UserModel.ts
@@ -0,0 +1,10 @@
+class User {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+export default User;
